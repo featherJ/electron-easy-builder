de8e0dc96116553cc754f5549a5079c796d628e9
Agua.L <438517073@qq.com>
2024-09-30 21:13:53 +0800

    提示改为英文

--- src/utils/elevate.ts
+++ src/utils/elevate.ts
@@ -1,5 +1,6 @@
 import { exec } from 'child_process';
 import { platform } from 'os';
+import { warn } from './log';
 
 // 检查是否拥有管理员权限
 function isElevated() {
@@ -19,14 +20,13 @@ export async function ensureElevated():Promise<void>{
     const isAdmin = await isElevated();
     return new Promise<void>((resolve,reject)=>{
         if (!isAdmin) {
-            console.log('当前没有管理员权限，正在重新以管理员身份运行脚本...');
+            warn("Administrator privileges not detected. Rerunning the script with elevated privileges...")
             // 使用 PowerShell 重新启动当前脚本，并申请管理员权限
             const scriptPath = process.argv[1]; // 当前脚本路径
             const args = process.argv.slice(2).join(' '); // 传递的参数
             const fullCommand = `node "${scriptPath}" ${args}`;
             exec(`powershell -Command "Start-Process cmd -ArgumentList '/k ${fullCommand}' -Verb RunAs"`);
         } else {
-            console.log('已经有管理员权限，继续执行脚本...');
             resolve();
         }
     });
